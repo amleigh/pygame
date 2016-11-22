09c39051db714edf4c81170bc51c0983553842dd
Amanda Leigh <amandaleighmcleod@comcast.net>
2016-11-22 16:01:11 -0500

    new additions

--- pygamecode.py
+++ pygamecode.py
@@ -1,2 +1,19 @@
 print ('Amanda McLeod')
 print('42936051')
+
+import pygame
+from pygame.locals import Rect, DOUBLEBUF, QUIT, K_ESCAPE, KEYDOWN, K_DOWN, \
+    K_LEFT, K_UP, K_RIGHT, KEYUP, K_LCTRL, K_RETURN, FULLSCREEN
+from pygame.locals import *
+
+pygame.init()
+X_MAX=800
+Y_MAX=600
+
+def main():
+	screen= pygame.display.set_mode((X_MAX, Y_MAX))
+	background = pygame.image.load("maze.png").convert()
+	screen.blit(background, [0, 0])
+
+
+main()
