bd30728496ab1b2169ce14eaca154513df09bf63
Amanda Leigh <amandaleighmcleod@comcast.net>
2016-11-24 16:33:55 -0500

    new material

--- pygamecode.py
+++ pygamecode.py
@@ -12,11 +12,8 @@ DELAY=100
 
 
 
-class Hoop(Sprite):
+class Hoop(pygame.sprite.Sprite):
   def __init__(self):
-    randX = randint(0, 600)
-    randY = randint(0, 400)
-    self.rect.center = (randX,randY)
     self.size = 150
     self.colour = (0, 0, 255)
     self.thickness = 1
@@ -24,29 +21,63 @@ class Hoop(Sprite):
     Sprite.__init__(self)
         self.rect = self.image.get_rect()
 
+	def move(self):
+		randX = randint(0, 600)
+    	randY = randint(0, 400)
+    	self.rect.center = (randX,randY)
+    	move_final= self.rect.center
+
+
 	def display(self):
-  		pygame.draw.circle(screen, self.colour, self.rect.center, self.size, self.thickness)
+  		pygame.draw.circle(self, self.colour, self.move, self.size, self.thickness)
 
 
-
-class Helicopter(Sprite):
+class Monster(pygame.sprite.Sprite): 
 	def __init__(self):
         Sprite.__init__(self)
         self.image = image.load("helicpter.gif").convert()
         self.rect = self.image.get_rect()
 
-    def move(self):
+    
 
-        
+class Helicopter(pygame.sprite.Sprite):
+	def __init__(self):
+        Sprite.__init__(self)
+        self.image = image.load("helicpter.gif").convert()
+        self.rect = self.image.get_rect()
+        self.score=0
+        self.dx=self.dy
+
+    def move(self, movement):
+    	v=10
+		if movement in (UP, DOWN):
+			self.dy = {UP: -v,
+                       DOWN: v}[direction]
+
+        if movement in (LEFT, RIGHT):
+            self.dx = {LEFT: -v,
+                       RIGHT: v}[direction]
+    def collision (self, hoop):
+    	self.score +=1
+    	return self.rect.colliderect(target)
+
+
+
+   
 
   		
 
 
 def main():
 	pygame.init()
+	target=Hoop()
+	heli= Helicopter()    
+
+	timer=pygame.time.Clock()
+
 	game_over = False 
 	screen= pygame.display.set_mode((800, 600))
-	pygame.display.set_caption("Move the Helicopter with the Up and Down arrows and don't hit the walls.")
+	pygame.display.set_caption("Move the Helicopter through as many hoops as possible before time runs out.")
 
 	background = pygame.Surface(screen.get_size())
 	background.fill((250, 250, 250))
@@ -57,6 +88,8 @@ def main():
 	pygame.display.flip()
 
 	while True:
+		clock.tick=30
+		mixer.Sound(#song).play()
     	e = event.poll()
     	if e.type == QUIT:
         	quit()
