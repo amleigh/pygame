ee4706c7db891962699fd220bd4b9aa071fcc0ff
Amanda Leigh <amandaleighmcleod@comcast.net>
2016-11-27 11:57:07 -0500

    adding music and other

--- pygamecode.py
+++ pygamecode.py
@@ -13,64 +13,58 @@ DELAY=100
 
 
 class Hoop(pygame.sprite.Sprite):
-  def __init__(self):
-    self.size = 150
-    self.colour = (0, 0, 255)
-    self.thickness = 1
-    
-    Sprite.__init__(self)
-        self.rect = self.image.get_rect()
+	def __init__(self):
+	    self.size = 150
+	    self.colour = (0, 0, 255)
+	    self.thickness = 1
+	    self.rect = self.image.get_rect()
 
 	def move(self):
 		randX = randint(0, 600)
-    	randY = randint(0, 400)
-    	self.rect.center = (randX,randY)
-    	move_final= self.rect.center
+		randY = randint(0, 400)
+		self.rect.center = (randX,randY)
+		move_final= self.rect.center
 
 
 	def display(self):
-  		pygame.draw.circle(self, self.colour, self.move, self.size, self.thickness)
+		pygame.draw.circle(self, self.colour, self.move, self.size, self.thickness)
 
 
 class Fire(pygame.sprite.Sprite): 
 	def __init__(self):
-        Sprite.__init__(self)
-        self.image = image.load("fire.gif").convert()
-        self.rect = self.image.get_rect()
+		self.image = image.load("fire.gif").convert()
+		self.rect = self.image.get_rect()
 
-    def appear(self):
-    	randX = randint(0, 600)
-    	randY = randint(0, 400)
-    	self.rect.copy = (randX,randY)
-    	appear_final= self.rect.copy
+	def appear(self):
+		randX = randint(0, 600)
+		randY = randint(0, 400)
+		self.rect.copy = (randX,randY)
+		appear_final= self.rect.copy
 
 
 class Helicopter(pygame.sprite.Sprite):
 	def __init__(self):
-        Sprite.__init__(self)
-        self.image = image.load("helicpter.gif").convert()
-        self.rect = self.image.get_rect()
-        self.score=0
-        self.dx=self.dy
+		self.image = image.load("helicpter.gif").convert()
+		self.rect = self.image.get_rect()
+		self.score=0
+		self.dx=self.dy
 
-    def move(self, movement):
-    	v=10
+	def move(self, movement):
+		v=10
 		if movement in (UP, DOWN):
-			self.dy = {UP: -v,
-                       DOWN: v}[direction]
+			self.dy = {UP: -v, DOWN: v}[direction]
 
-        if movement in (LEFT, RIGHT):
-            self.dx = {LEFT: -v,
-                       RIGHT: v}[direction]
-    
-    def collision (self, hoop):
-    	self.score +=1
-    	return self.rect.colliderect(hoop)
+		if movement in (LEFT, RIGHT):
+			self.dx = {LEFT: -v,RIGHT: v}[direction]
+
+	def collision (self, hoop):
+		self.score +=1
+		return self.rect.colliderect(hoop)
 
 
-    def dead(self, death):
-    	if self.rect.colliderect(death):
-    		self.kill()
+	def dead(self, death):
+		if self.rect.colliderect(death):
+			self.kill()
 
    
 
@@ -98,46 +92,57 @@ def main():
 	screen.blit(background, (0, 0))
 	pygame.display.flip()
 
+
+
+	pygame.mixer.music.play("heli_sound.wav")
+
 	while True:
 		clock.tick(45)
-		#mixer.Sound(#song).play()
-    	for occurance in pygame.event.get():
-           	if occurance.type == QUIT or occurance.key == K_ESCAPE:
-           		sys.exit()
+
+		for occurance in pygame.event.get():
+			if occurance.type == QUIT or occurance.key == K_ESCAPE:
+				sys.exit()
 			if occurance.type == KEYDOWN:
-                if occurance.key == K_DOWN:
-                    heli.move(DOWN)
-                if occurance.key == K_LEFT:
-                    heli.move(LEFT)
-                if occurance.key == K_RIGHT:
-                    heli.move(RIGHT)
-                if occurance.key == K_UP:
-                	heli.move(UP)
-            if occurance.type == KEYUP:
-                    if occurance.key == K_DOWN:
-                        heli.move(DOWN)
-                    if occurance.key == K_LEFT:
-                        heli.move(LEFT)
-                    if occurance.key == K_RIGHT:
-                        heli.move(RIGHT)
-                    if occurance.key == K_UP:
-                        heli.move(UP)
-    	
-    	if pygame.sprite.spritecollide(heli, obstical)= True:
+				if occurance.key == K_DOWN:
+					heli.move(DOWN)
+				if occurance.key == K_LEFT:
+					heli.move(LEFT)
+				if occurance.key == K_RIGHT:
+					heli.move(RIGHT)
+				if occurance.key == K_UP:
+					heli.move(UP)
+			if occurance.type == KEYUP:
+				if occurance.key == K_DOWN:
+					heli.move(DOWN)
+				if occurance.key == K_LEFT:
+					heli.move(LEFT)
+				if occurance.key == K_RIGHT:
+					heli.move(RIGHT)
+					if occurance.key == K_UP:
+						heli.move(UP)
+
+		if pygame.sprite.spritecollide(heli, obstical) = True:
     		heli.dead(obstical)
     		game_over=True
-    		print("GAME OVER")
+    		end_text= fon.render("GAME OVER!! Score {}".format(heli.collision.score), 1, (250, 250, 250))
+    		text=screen.blit(end_text, (0, 0)))
+    		print(text)
     		sys.exit()
     	
-    	if pygame.sprite.spritecollide(heli, target)= True:
+    	if pygame.sprite.spritecollide(heli, target) = True:
     		target.move()
     		heli.collision(target)
     		obstical.appear()
-    		scoretext = fon.render("Score {0}".format(heli.collision.score), 1, (0,0,0))
-    		screen.blit(scoretext, (5, 10))
+    		scoretext = fon.render("Score {0}".format(heli.collision.score), 1, (250, 250, 250))
+    		screen.blit(scoretext, (0, 0)))
 
     	if game_over == True:
+    		final_text= fon.render("GAME OVER!! Score {}".format(heli.collision.score), 1, (250, 250, 250))
+    		fin=screen.blit(final_text, (0, 0)))
+    		print (fin)
 
+    
+    	pygame.display.flip()
 #music
 #display score and time
 #set scoer to zero to start
